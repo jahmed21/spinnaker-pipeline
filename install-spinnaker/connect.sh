1d6c560859ed42df042275bdb5d92bf96b7613b8
manikann <manikann@gmail.com>
2018-09-21 09:22:26 +0800

    Removed dockerhub registry, added namespace/org for gcr

--- install-spinnaker/connect.sh
+++ install-spinnaker/connect.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 DECK_POD=$(kubectl get pods --namespace spinnaker -l "cluster=spin-deck" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward --namespace spinnaker $DECK_POD 9000
+kubectl port-forward --namespace spinnaker $DECK_POD 9000 > /dev/null 
